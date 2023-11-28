475bd7cecc43a038c6267d3c0a9567f856ee9027
dzkm <13489382+dzkm@users.noreply.github.com>
2023-11-27 22:18:55 -0300

    feat(app): altered the callback of the room routes

--- web/html/src/Routes/RoomsRoutes.php
+++ web/html/src/Routes/RoomsRoutes.php
@@ -4,29 +4,19 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\RequestInterface as Request;
 use Slim\App;
 use App\Controllers\RoomController;
-use Medoo\Medoo;
 return function (App $app) {
     $container = $app->getContainer();
+    
+    $app->get("/rooms", [RoomController::class, 'getAllRooms']);
 
-    $app->get('/rooms', function (Request $request, Response $response) use ($container){
-        $controller = $container->get(RoomController::class);
-        $response->getBody()->write(json_encode($controller->getAllRooms($request, $response)));
-        return $response;
-    });
-    $app->get('/rooms/{id}', function (Request $request, Response $response) {
-        $response->getBody()->write('<a href="/rooms"> try /casa/automatica</a>');
-        return $response;
-    });
-    $app->post('/rooms', function (Request $request, Response $response) {
-        $response->getBody()->write('<a href="/rooms"> try /casa/automatica</a>');
-        return $response;
-    });
+    $app->get("/rooms/{id}", [RoomController::class, 'getRoomByID']);
+
+    $app->post("/rooms", [RoomController::class, 'createRoom']);
+
     $app->put('/rooms/{id}', function (Request $request, Response $response) {
         $response->getBody()->write('<a href="/rooms"> try /casa/automatica</a>');
         return $response;
     });
-    $app->delete('/rooms/{id}', function (Request $request, Response $response) {
-        $response->getBody()->write('<a href="/rooms"> try /casa/automatica</a>');
-        return $response;
-    });
+    $app->delete('/rooms/{id}', [RoomController::class, 'deleteRoom']);
+
 };
