f5318e11849385354b36d2220a482d079b23918b
dzkm <13489382+dzkm@users.noreply.github.com>
2023-11-27 21:12:00 -0300

    feat(app): added Room model

--- web/html/src/Model/Room.php
+++ web/html/src/Model/Room.php
@@ -1,13 +1,31 @@
 <?php
 namespace App\Models;
+use DateTime;
 
 class Room{
-    public function __construct(private int $id, private string $description){
+    private int $id;
+    private int $userID;
+    private int $houseID;
+    private string $description;
+    private DateTime $created_at;
+    private DateTime $updated_at;
+    private DateTime $deleted_at;
+    private bool $deleted;
+
+    
+    public function __construct(int $id, string $description){
     }
     public function getId(): int{
         return $this->id;
     }
     public function getDescription(): string{
         return $this->description;
+    }
+    public function toMap(): array{
+        return [
+            "id"=> $this->id,
+            "description"=> $this->description,
+
+        ];
     }
 }
