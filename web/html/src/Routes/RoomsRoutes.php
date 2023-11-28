a3340f009f8f27b67e4e04322270d51b2775f7d0
dzkm <13489382+dzkm@users.noreply.github.com>
2023-11-27 21:12:13 -0300

    feat(php): added room routes

--- web/html/src/Routes/RoomsRoutes.php
+++ web/html/src/Routes/RoomsRoutes.php
@@ -1,10 +1,16 @@
 <?php
+declare(strict_types=1);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\RequestInterface as Request;
 use Slim\App;
+use App\Controllers\RoomController;
+use Medoo\Medoo;
 return function (App $app) {
-    $app->get('/rooms', function (Request $request, Response $response) {
-        $response->getBody()->write('<a href="/rooms"> try /casa/automatica</a>');
+    $container = $app->getContainer();
+
+    $app->get('/rooms', function (Request $request, Response $response) use ($container){
+        $controller = $container->get(RoomController::class);
+        $response->getBody()->write(json_encode($controller->getAllRooms($request, $response)));
         return $response;
     });
     $app->get('/rooms/{id}', function (Request $request, Response $response) {
